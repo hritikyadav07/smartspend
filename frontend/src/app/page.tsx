20e40f0eae7b68c31b4502176208406c4a01a8cc
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2025-05-02 10:22:15 +0530

    set up of hero ui done

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,27 @@
-
+"use client";
 export default function Home() {
+ 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="text-3xl font-bold underline text-center m-10">
+      My Name is <span className="text-blue-500">Anthony Gonzalviz</span>
+      <br />
+      <ThemeChanger/>
     </h1>
+  )
+}
+
+function ThemeChanger(){
+  const changeTheme = () => {
+    const value = document.querySelector("html");
+    if (value) {
+      value.classList.toggle("dark");
+      value.classList.toggle("light");
+    }
+  };
+  return (
+    <>
+    <button onClick={changeTheme} className="">Change Theme</button>
+    </>
   )
 }
---
+++ frontend/src/app/providers.jsx
@@ -0,0 +1,13 @@
+// app/providers.tsx
+'use client'
+import React from 'react'
+
+import {HeroUIProvider} from '@heroui/react'
+
+export function Providers({children}) {
+  return (
+    <HeroUIProvider>
+      {children}
+    </HeroUIProvider>
+  )
+}
