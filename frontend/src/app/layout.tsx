8961bbdcff6d89157fef4bf339ecf92b38bd7976
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2025-05-07 18:06:24 +0530

    added login and sign up with clerk

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,17 @@
 import "./globals.css";
 import {Providers} from "./providers";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
-    <html lang="en" className='dark'>
-      <body>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className='dark'>
+        <body>
+          <Providers>
+            {children}
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
